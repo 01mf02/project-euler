2ce3cec1b770bb6229c860815a9379b4875fa4cb
Michael Färber <01mf02@gmail.com>
2015-03-08 22:02:55 +0100

    Simplify a bit.

--- projectEuler.hs
+++ projectEuler.hs
@@ -663,7 +663,7 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 -}
 
 numOfDigits = foldl (\ acc x -> x + 10*acc) 0
-problem24 = numOfDigits $ sort (permutations [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) !! 999999
+problem24 = numOfDigits $ sort (permutations [0 .. 9]) !! 999999
 
 
 {-
